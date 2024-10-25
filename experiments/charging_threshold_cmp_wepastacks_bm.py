50ed0f0c372e507622cc04ce4ba4dfc15b2f46f3
j4n1k <janik.bischoff@gmx.de>
2024-10-25 16:05:39 +0200

    setup partitions comp loop

--- experiments/charging_threshold_cmp_wepastacks_bm.py
+++ experiments/charging_threshold_cmp_wepastacks_bm.py
@@ -20,88 +20,69 @@ from slapstack_controls.storage_policies import (ClassBasedPopularity,
 from slapstack_controls.charging_policies import (FixedChargePolicy,
                                                   RandomChargePolicy,
                                                   FullChargePolicy,
-                                                  ChargingPolicy)
-
-from stable_baselines3 import DQN
-import os
-import subprocess
-
+                                                  ChargingPolicy, LowTHChargePolicy)
 
 
 def get_episode_env(sim_parameters: SimulationParameters,
-                    storage_strategy: StoragePolicy,
                     log_frequency: int, nr_zones: int,
                     logfile_name: str,
-                    log_dir='./result_data/'):
+                    log_dir: str,
+                    action_converters: list,
+                    partitions: list):
+    if partitions == None:
+        partitions = [None]
     seeds = [56513]
-    if isinstance(sim_parameters.initial_pallets_storage_strategy,
-                  ClassBasedPopularity):
-        sim_parameters.initial_pallets_storage_strategy = ClassBasedPopularity(
-            retrieval_orders_only=False,
-            future_counts=True,
-            init=True,
-            n_zones=nr_zones
-        )
     return SlapEnv(
-        sim_parameters, seeds,
+        sim_parameters, seeds, partitions,
         logger=ExperimentLogger(
             filepath=log_dir,
             n_steps_between_saves=log_frequency,
             nr_zones=nr_zones,
             logfile_name=logfile_name),
-        action_converters=[BatchFIFO(),
-                           storage_strategy])
+        action_converters=action_converters)
 
 
 def _init_run_loop(simulation_parameters,
-                   charging_strategy: Union[FixedChargePolicy|RandomChargePolicy],
-                   storage_strategy: StoragePolicy,
-                   log_dir):
-    th = None
-    if isinstance(charging_strategy, FixedChargePolicy):
-        th = charging_strategy.charging_threshold
-    elif isinstance(charging_strategy, RandomChargePolicy):
-        th = "random"
-    if hasattr(storage_strategy, 'n_zones'):
-        environment: SlapEnv = get_episode_env(
-            sim_parameters=simulation_parameters,
-            storage_strategy=storage_strategy,
-            log_frequency=1000,
-            nr_zones=storage_strategy.n_zones, log_dir=log_dir,
-            logfile_name=f'{storage_strategy.name}_{charging_strategy.name}_th{th}')
-    else:
-        environment: SlapEnv = get_episode_env(
-            sim_parameters=simulation_parameters,
-            storage_strategy=storage_strategy,
-            log_frequency=1000, nr_zones=3, log_dir=log_dir,
-            logfile_name=f'{storage_strategy.name}_{charging_strategy.name}_th{th}')
-    loop_controls = LoopControl(environment, steps_per_episode=120)
-    # state.state_cache.perform_sanity_check()
+                   log_dir,
+                   action_converters: list,
+                   steps_per_episode: int,
+                   partitions: list,
+                   charging_strategy: ChargingPolicy):
+    pt = simulation_parameters.use_case_partition_to_use
+    environment: SlapEnv = get_episode_env(
+        sim_parameters=simulation_parameters,
+        log_frequency=1000, nr_zones=3, log_dir=log_dir,
+        logfile_name=f'pt_{pt}_COL_{charging_strategy.name}_th{charging_strategy.charging_threshold}',
+        action_converters=action_converters,
+        partitions=partitions)
+    loop_controls = LoopControl(environment, steps_per_episode=steps_per_episode)
     return environment, loop_controls
 
 
 def run_episode(simulation_parameters: SimulationParameters,
-                charging_strategy: Union[FixedChargePolicy|RandomChargePolicy],
-                storage_strategy: StoragePolicy,
+                charging_check_strategy,
+                partitions: list,
                 print_freq=0, stop_condition=False,
-                log_dir='./result_data_charging/', steps_per_episode=None):
+                log_dir='', steps_per_episode=None,
+                action_converters=None):
     env, loop_controls = _init_run_loop(
-        simulation_parameters, charging_strategy, storage_strategy, log_dir)
+        simulation_parameters, log_dir, action_converters, steps_per_episode, partitions, charging_check_strategy)
     parametrization_failure = False
     start = time.time()
     while not loop_controls.done:
-        if env.core_env.decision_mode == "charging":
+        decision_mode = env.core_env.decision_mode
+        if decision_mode == "charging_check" or decision_mode == "charging":
             prev_event = env.core_env.previous_event
-            action = charging_strategy.get_action(loop_controls.state,
-                                                  agv_id=prev_event.agv_id)
+            action = charging_check_strategy.get_action(loop_controls.state,
+                                                        agv_id=prev_event.agv.id)
         else:
             if env.done_during_init:
                 raise ValueError("Sim ended during init")
             raise ValueError
-        output, reward, loop_controls.done, info = env.step(action)
+        output, reward, loop_controls.done, info, _ = env.step(action)
         if print_freq and loop_controls.n_decisions % print_freq == 0:
             ExperimentLogger.print_episode_info(
-                charging_strategy.name, start, loop_controls.n_decisions,
+                charging_check_strategy.name, start, loop_controls.n_decisions,
                 loop_controls.state)
             # state.state_cache.perform_sanity_check()
         loop_controls.n_decisions += 1
@@ -110,13 +91,8 @@ def run_episode(simulation_parameters: SimulationParameters,
         if loop_controls.done:
             parametrization_failure = True
             env.core_env.logger.write_logs()
-        # if not loop_controls.done and stop_condition:
-        #     loop_controls.done = loop_controls.stop_prematurely()
-        #     if loop_controls.done:
-        #         parametrization_failure = True
-        #         env.core_env.logger.write_logs()
     ExperimentLogger.print_episode_info(
-        charging_strategy.name, start, loop_controls.n_decisions,
+        charging_check_strategy.name, start, loop_controls.n_decisions,
         loop_controls.state)
     return parametrization_failure
 
@@ -127,9 +103,9 @@ def get_charging_strategies():
     charging_strategies += [
         # FixedChargePolicy(30),
         # FixedChargePolicy(40),
-        FixedChargePolicy(50),
+        # FixedChargePolicy(50),
         # FixedChargePolicy(60),
-        # FixedChargePolicy(70),
+        FixedChargePolicy(70),
         # FixedChargePolicy(80),
         # FixedChargePolicy(90),
         # FixedChargePolicy(100),
@@ -139,121 +115,65 @@ def get_charging_strategies():
     return charging_strategies
 
 
-def get_storage_strategies(nr_zones: List[int]):
+def get_storage_strategies():
     storage_strategies = []
-    # for n_zone in nr_zones:
-    #     storage_strategies += [
-    #         ClassBasedCycleTime(
-    #             n_orders=10000, recalculation_steps=1000, n_zones=n_zone),
-    #         ClassBasedPopularity(
-    #             retrieval_orders_only=False, n_zones=n_zone,
-    #             future_counts=True,
-    #             name=f'allOrdersPopularity_future_z{n_zone}'),
-    #         ClassBasedPopularity(
-    #             retrieval_orders_only=True, n_zones=n_zone,
-    #             future_counts=True,
-    #             name=f'retrievalPopularity_future_z{n_zone}'),
-    #         ClassBasedPopularity(
-    #             retrieval_orders_only=False, n_zones=n_zone,
-    #             future_counts=False, n_orders=10000, recalculation_steps=1000,
-    #             name=f'allOrdersPopularity_past_z{n_zone}'),
-    #         ClassBasedPopularity(
-    #             retrieval_orders_only=True, n_zones=n_zone,
-    #             future_counts=False, n_orders=10000, recalculation_steps=1000,
-    #             name=f'retrievalPopularity_past_z{n_zone}')
-    #     ]
     storage_strategies += [
-        # ClosestOpenLocation(very_greedy=True),
        ClosestOpenLocation(very_greedy=False),
     ]
     return storage_strategies
 
 
-storage_policies = get_storage_strategies([5])
+storage_policies = get_storage_strategies()
 charging_strategies = get_charging_strategies()
 
-params = SimulationParameters(
-                use_case="wepastacks_bm",
-                use_case_n_partitions=1,
-                use_case_partition_to_use=0,
-                n_agvs=40,
-                generate_orders=False,
-                verbose=False,
-                resetting=False,
-                # initial_pallets_storage_strategy=ClassBasedPopularity(
-                #     retrieval_orders_only=False,
-                #     future_counts=True,
-                #     init=True,
-                #     # n_zones changes dynamically based on the slap strategy
-                #     # in get_episode_env
-                #     n_zones=2
-                # ),
-                initial_pallets_storage_strategy=ConstantTimeGreedyPolicy(),
-                pure_lanes=True,
-                n_levels=3,
-                # https://logisticsinside.eu/speed-of-warehouse-trucks/
-                agv_speed=2,
-                unit_distance=1.4,
-                pallet_shift_penalty_factor=20,  # in seconds
-                compute_feature_trackers=True,
-                charging_thresholds=[40, 50, 60, 70, 80],
-            )
-
 if __name__ == '__main__':
-    # hashseed = os.getenv("PYTHONHASHSEED")
-    # if not hashseed:
-    #     os.environ["PYTHONHASHSEED"] = "0"
-    #     os.execv(sys.executable, [sys.executable] + sys.argv)
-
     st = [ClosestOpenLocation(very_greedy=False)]
     n_charging_strategies = len(charging_strategies)
     n_storage_strategies = len(storage_policies)
-    # for i in range(n_charging_strategies):
-    #     print(f"Running sim with and charging strategy: {charging_strategies[i].name}")
-    #     run_episode(
-    #             simulation_parameters=params,
-    #             charging_strategy=charging_strategies[i],
-    #             storage_strategy=ClosestOpenLocation(very_greedy=False),
-    #             print_freq=10000,
-    #             steps_per_episode=120,
-    #             log_dir=
-    #             f'./result_data_charging_wepa/test2'
-    #             )
-
-    # for i in range(n_storage_strategies):
-    #     for j in range(n_charging_strategies):
-    #         print(f"Running sim with storage strategy: {storage_policies[i].name} "
-    #               f"and charging strategy: {charging_strategies[j].name}")
-    #         run_episode(
-    #                 simulation_parameters=params,
-    #                 charging_strategy=charging_strategies[j],
-    #                 storage_strategy=storage_policies[i],
-    #                 print_freq=100,
-    #                 steps_per_episode=120,
-    #                 log_dir=
-    #                 f'./result_data_charging_wepa'
-    #                 )
-
     # parallel charging strat
-    # parallelize_heterogeneously(
-    #     [run_episode] * n_charging_strategies,
-    #     list(zip([params] * n_charging_strategies,                    # params
-    #              charging_strategies,
-    #              st,  # policy
-    #              [0] * n_charging_strategies,                         # print_freq
-    #              [False] * n_charging_strategies,                     # warm_start
-    #              ['./result_data_charging_wepa'] * n_charging_strategies,
-    #              )))
+    n_partitions = 20
+    for pt in range(n_partitions):
+        params = SimulationParameters(
+            use_case="wepastacks_bm",
+            use_case_n_partitions=n_partitions,
+            use_case_partition_to_use=pt,
+            n_agvs=40,
+            generate_orders=False,
+            verbose=False,
+            resetting=False,
+            initial_pallets_storage_strategy=ConstantTimeGreedyPolicy(),
+            pure_lanes=True,
+            n_levels=3,
+            # https://logisticsinside.eu/speed-of-warehouse-trucks/
+            agv_speed=2,
+            unit_distance=1.4,
+            pallet_shift_penalty_factor=20,  # in seconds
+            compute_feature_trackers=True,
+            charging_thresholds=[40, 50, 60, 70, 80],
+        )
+        parallelize_heterogeneously(
+            [run_episode] * n_charging_strategies,
+            list(zip([params] * n_charging_strategies,                    # params
+                     charging_strategies,
+                     [[None]] * n_charging_strategies,  # partitions to cycle
+                     [0] * n_charging_strategies,                         # print_freq
+                     [False] * n_charging_strategies,   # stop condition
+                     ['./result_data_charging_wepa'] * n_charging_strategies,
+                     [None] * n_charging_strategies,
+                     [[BatchFIFO(),
+                      ClosestOpenLocation(very_greedy=False),
+                      LowTHChargePolicy(20)]] * n_charging_strategies
+                     )))
 
     # Strategies with charging
-    all_combinations = [
-        (params, charging_strategies[j], storage_policies[i], 100, 120, './result_data_charging_wepa')
-        for i in range(n_storage_strategies)
-        for j in range(n_charging_strategies)
-    ]
-    parallelize_heterogeneously(
-        [run_episode] * len(all_combinations),
-        all_combinations)
+    # all_combinations = [
+    #     (params, charging_strategies[j], [0, 1, 2, 3], 100, 120, './result_data_charging_pt_wepa')
+    #     for i in range(n_storage_strategies)
+    #     for j in range(n_charging_strategies)
+    # ]
+    # parallelize_heterogeneously(
+    #     [run_episode] * len(all_combinations),
+    #     all_combinations)
 
     # Old loop
     # parallelize_heterogeneously(
